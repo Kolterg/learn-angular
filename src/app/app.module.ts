8f2d169ea2d0e6596abdf8b1b7592ed7cd278b57
Kolterg <mark.reta99@gmail.com>
2021-05-29 23:54:55 +0300

    Classwork1 in process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './component/cars/cars.component';
-import { UsersComponent } from './component/users/users.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
