6d055aeb3ae9010beaacff38e72815ef2413b4d4
Kolterg <mark.reta99@gmail.com>
2021-05-29 23:50:23 +0300

    Create branch Classwork1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './component/cars/cars.component';
+import { UsersComponent } from './component/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
