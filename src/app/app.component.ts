740f3ac14a4ac92ce900f800fcca076e49eab5e3
Kolterg <mark.reta99@gmail.com>
2021-05-30 12:46:19 +0300

    Create branch Classwork1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// 2
+// выводите всех юзеров, под каждым юзером его посты (все разделяете на компоненты, делаете сервисы и интерфейсы)
+//
+// кто не знает как с плейсхолдера вытянуть массив постов по id юзера:
+// http://jsonplaceholder.typicode.com/posts?usersId=1 посты юзера с id 1
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
