1da8bbf11875fbc72301393df361cfc5feb8a776
Kolterg <mark.reta99@gmail.com>
2021-06-04 11:52:16 +0300

    Complete Classwork2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+
+let routes: Routes = [
+  {path: 'users', component: UsersComponent, children: [
+      {path: ':id', component: UserDetailsComponent}
+    ]},
+  {path: 'posts', component: PostsComponent, children: [
+      {path: ':id', component: PostDetailsComponent}
+    ]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    UserComponent,
+    PostComponent,
+    UserDetailsComponent,
+    PostDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../modules/User";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.url);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.httpClient.get<User>(this.url + '/' + id)
+  }
+}
