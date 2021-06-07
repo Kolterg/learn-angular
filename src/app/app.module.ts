94a7004d1dfd72deaa33b8d921efa63b1be83d9b
Kolterg <mark.reta99@gmail.com>
2021-06-07 18:54:30 +0300

    Complete Homework3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+let routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'users', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
+  {path: 'posts', loadChildren: () => import('./modules/post/post.module').then(m => m.PostModule)}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
