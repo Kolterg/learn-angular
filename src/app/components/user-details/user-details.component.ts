363f0c8dfae940f913ed9384aef3e6985f1cefa0
Kolterg <mark.reta99@gmail.com>
2021-06-14 14:43:20 +0300

    Complete Classwork4.

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {User} from "../../modules/User";
+import {User} from "../../models/User";
 import {DataTransferService} from "../../services/data-transfer.service";
 
 @Component({
